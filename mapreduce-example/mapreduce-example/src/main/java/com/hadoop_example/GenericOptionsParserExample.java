066970d7c3a22be11937b7fd9742b0f3178d9b29
Jounghoon Kim <kjh97507@gmail.com>
2022-10-01 01:37:39 +0800

    Tool runner and GenericOptionsParser example

---
+++ mapreduce-example/mapreduce-example/src/main/java/com/hadoop_example/GenericOptionsParserExample.java
@@ -0,0 +1,25 @@
+package com.hadoop_example;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class GenericOptionsParserExample {
+    public static void main(String[] args) throws IOException{
+        
+        System.out.println(Arrays.toString(args));
+        Configuration conf = new Configuration();
+        GenericOptionsParser OptionsParser = new GenericOptionsParser(conf, args);
+        String value1 = conf.get("mapreduce.map.memory.mb");
+        Boolean value2 = conf.getBoolean("job.test", false);
+        System.out.println("value1"+value1+ "& value2"+ value2);
+        String [] remainingArgs = OptionsParser.getRemainingArgs();
+        System.out.println(Arrays.toString(remainingArgs));
+
+
+
+
+        
+    }
+}
